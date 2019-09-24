0f3cf9cc416e23babcf089a5bde7da7097ab589c
Michael <2015-github@red0.red>
2019-09-24 15:33:06 +0200

    Merge new VueCli Setup

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './plugins/router.js'
-import vuetify from './plugins/vuetify.js';
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'font-awesome/css/font-awesome.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   vuetify,
-  render: h => h(App),
-  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
+  render: h => h(App)
 }).$mount('#app')
