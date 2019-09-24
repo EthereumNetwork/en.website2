a9045a7b23e3d1d086be2dde891ab9e7e7f663ac
Michael <2015-github@red0.red>
2019-09-24 11:10:45 +0200

    Add Express History

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const history = require('connect-history-api-fallback')
 
+// history between requests and static files to catch client-side route paths
+app.use(history())
+
+// serving index.html and chunks, client-side routes handled by Vue router and history
 app.use(express.static('./dist'))
-
 
 
 app.listen(3000, () => console.log(`Example app listening on port 3000!`))
